ab693a370274be58be36359b6a3579ac8dd0b991
lbroudoux <laurent.broudoux@gmail.com>
2014-01-18 18:58:17 +0100

    Adding license header where appropriate

--- addon-layers-repository-elasticsearch/src/main/java/com/github/lbroudoux/roo/addon/layers/repository/elasticsearch/RepositoryElasticsearchMetadataProvider.java
+++ addon-layers-repository-elasticsearch/src/main/java/com/github/lbroudoux/roo/addon/layers/repository/elasticsearch/RepositoryElasticsearchMetadataProvider.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Laurent Broudoux (the "Author") under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Author licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.github.lbroudoux.roo.addon.layers.repository.elasticsearch;
 
 import java.util.LinkedHashMap;
@@ -23,7 +41,6 @@ import org.springframework.roo.project.LogicalPath;
 /**
  * Provides {@link RepositoryElasticsearchMetadata}. This type is called by Roo to retrieve the metadata for this add-on.
  * @author Laurent Broudoux
- * @since 1.2
  */
 @Component(immediate = true)
 @Service
